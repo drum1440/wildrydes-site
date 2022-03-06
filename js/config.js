94f9734e966101fb8f990acf15afab27e49b125e
drum1440 <drum1440@hotmail.com>
2022-03-05 19:18:16 -0600

    updated config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_Vq0T5BmIX', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6qprc7pno6af7ujn2s5ls09lof', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
